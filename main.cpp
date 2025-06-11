552f7ee118d13c2efb6bea87bb49def382e42570
Iain <iainwinter1@gmail.com>
2025-06-10 22:04:44 -0400

    adding needed third party stuff

--- main.cpp
+++ main.cpp
@@ -1,41 +1,10 @@
-#include <iostream>
-#include <fstream>
-#include <variant>
-#include <typeinfo>
-#include <optional>
-#include <chrono>
-#include <sstream>
-
 #include "byte_code_vm_debugger.h"
 #include "compiler.h"
 
 #include "test.h"
 
-void print(const std::vector<int>& stack) {
-    printf("Stack:\n");
-    for (auto x : stack) {
-        printf("%d\n", x);
-    }
-}
-
-void print(const std::unordered_map<std::string, int>& variables) {
-    printf("Variables:\n");
-    for (auto x : variables) {
-        printf("%s : %d\n", x.first.c_str(), x.second);
-    }
-}
-
-std::chrono::high_resolution_clock::time_point g_start_time;
-
-void time_start() {
-    g_start_time = std::chrono::high_resolution_clock::now();
-}
-
-void time_end(const std::string& label) {
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration<double, std::milli>(end_time - g_start_time).count();
-    std::cout << label << " took " << duration << " ms\n";
-}
+#include <fstream>
+#include <sstream>
 
 int main() {
     run_tests();
@@ -48,15 +17,12 @@ int main() {
         x = buffer.str();
     }
     
-    time_start();
     CompilationResults results = compile(x);
-    time_end("Compilation");
 
     if (results.error.type != CompilationErrorType::NONE) {
         return 1;
     }
 
-    time_start();
     ByteCodeVm vm(results.operations, results.main_code_index);
     ByteCodeVmDebugger debugger(vm);
 
@@ -72,7 +38,5 @@ int main() {
     debugger.breakpoint_step();
     debugger.breakpoint_continue();
     
-    time_end("Execution");
-
     return 0;
 }
